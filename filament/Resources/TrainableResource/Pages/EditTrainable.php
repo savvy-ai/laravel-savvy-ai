10d09709ef1bc21bf42d5018e202c7368680fceb
Steven <steven@modcreativeinc.com>
2023-05-05 12:00:56 -0500

    Add getRelationManagers method to edit page for trainable filament resource

--- filament/Resources/TrainableResource/Pages/EditTrainable.php
+++ filament/Resources/TrainableResource/Pages/EditTrainable.php
@@ -16,4 +16,9 @@ class EditTrainable extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRelationManagers(): array
+    {
+        return [];
+    }
 }
