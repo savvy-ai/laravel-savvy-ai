ce36aaf863be6e9631577de2bc52cb357c8ff046
Steven <steven@modcreativeinc.com>
2023-05-11 21:27:42 -0500

    Add basic seeder to package for admin user

---
+++ database/seeders/SavvyAISeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $user = User::query()->create([
+            'name' => 'Admin',
+            'email' => 'admin@modcreativeinc.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+        ]);
+
+        /**
+         * Add desired Trainable, Chatbot, Agent, and Dialogue seeds here.
+         */
+    }
+}
