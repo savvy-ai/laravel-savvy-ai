70b581d11f05534a12a32311459c396f8a475916
Steven <steven@modcreativeinc.com>
2023-05-15 22:09:08 -0500

    Reset training page after successful training by redirecting back to itself

--- filament/Resources/TrainableResource/Pages/Training.php
+++ filament/Resources/TrainableResource/Pages/Training.php
@@ -132,6 +132,6 @@ class Training extends Page
             'is_training' => false,
         ]);
 
-        return redirect()->back();
+        return redirect()->route('filament.resources.trainables.training', $this->record);
     }
 }
