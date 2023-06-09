0c0a2555e69e1478dc88a456d698fbc6d60995d3
Steven <steven@modcreativeinc.com>
2023-06-09 14:35:53 -0500

    Add improved default messages to exceptions

--- src/Exceptions/DelegateNotFoundException.php
+++ src/Exceptions/DelegateNotFoundException.php
@@ -4,7 +4,7 @@ namespace SavvyAI\Exceptions;
 
 class DelegateNotFoundException extends \Exception
 {
-    public function __construct(string $message = 'I am sorry, but I am not equipped to answer that question.', int $code = 0, \Throwable $previous = null)
+    public function __construct(string $message = 'I\'m not equipped to answer that question, and I apologize for any inconvenience.', int $code = 0, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
